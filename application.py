feecf3b186d493a56a8a4a4ea5b00fbe97e2ef62
sidxlfc@gmail.com <sid.xlfc@gmail.com>
2016-06-20 21:29:08 -0500

    login test

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def Welcome():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return "Login"
+
 if __name__ == "__main__":
     app.run()
 
